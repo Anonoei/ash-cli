bf9fa0345c96b5d9f2394cf834f8cdb9de4ac4a2
Anonoei <dev@anonoei.com>
2024-11-10 16:22:04 -1000

    Restructure, getting ready for extras

--- a.sh
+++ a.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # ASH CLI
 
+ASH_VERSION="0.3.1"
+ASH_CONFIG="${HOME}/.config/ash"
+ASH_EXTRAS=()
+
 function ash_config_read {
     (grep -E "^${2}=" -m 1 "${1}" 2>/dev/null || echo "VAR=__UNDEFINED__") | head -n 1 | cut -d '=' -f 2-;
 }
@@ -13,29 +17,26 @@ function ash_config_get {
     printf -- "%s" "${val}";
 }
 
+ASH_ROOT="$(ash_config_get "PATH_ROOT" | sed 's/~/$HOME/g')"
+ASH_ROOT="$(eval echo $ASH_ROOT)"
+
+if [[ "$ASH_ROOT" == "__UNDEFINED__" ]] ; then
+    echo "Failed to get PATH_ROOT"
+    return
+fi
+
+if [[ ! -d "$ASH_ROOT" ]]; then
+    echo "Failed to find ASH src!"
+    return
+fi
+
+mkdir -p $ASH_CONFIG/plugins
+
+source $ASH_ROOT/ash.d/escape
+source $ASH_ROOT/logger "ASH" -l "TRACE"
+
+source $ASH_ROOT/install
+
 function ash {
-    export ASH_VERSION="0.3.1"
-
-    export ASH_CONFIG="${HOME}/.config/ash"
-
-    export ASH_ROOT="$(ash_config_get "PATH_ROOT" | sed 's/~/$HOME/g')"
-    export ASH_ROOT="$(eval echo $ASH_ROOT)"
-
-    #export ASH_PLUGS=($(ash_config_get "PLUGINS"))
-
-    if [[ ! -d "$ASH_ROOT" ]]; then
-        echo "Failed to find ASH src!"
-        exit 1
-    fi
-
-    source $ASH_ROOT/ash.d/main
-
-    mkdir -p $ASH_CONFIG/plugins
-
-    echo "ASH:"
-    echo " ASH_VERSION: $ASH_VERSION"
-    echo " ASH_ROOT: $ASH_ROOT"
-    echo " ASH_PLUGS: (${ASH_PLUGS[@]})"
-
-    _ash $*
+    source $ASH_ROOT/ash.d/main $*
 }
